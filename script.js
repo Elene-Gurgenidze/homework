2bba1051c99e267434186cb6bd5d00a843ede4fe
Elene-Gurgenidze <elenegurgenidze81@gmail.com>
2025-05-18 22:13:51 +0400

    პირველი კომიტი

---
+++ script.js
@@ -0,0 +1,12 @@
+const changidtext = document.getElementById("changed-text");
+
+function dashboard() {
+    changidtext.innerHTML = "Dashboard";
+}
+function invertors() {
+    changidtext.innerHTML = "inventors";
+}
+
+function funds() {
+    changidtext.innerHTML = "Fuds"
+}
